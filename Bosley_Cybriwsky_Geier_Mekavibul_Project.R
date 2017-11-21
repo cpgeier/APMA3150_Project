e0476f593c7bb9f188e2f47876fe8e30a1a053c7
cpgeier <cpg3rb@virginia.edu>
2017-11-21 17:45:56 -0500

    change var name

--- Bosley_Cybriwsky_Geier_Mekavibul_Project.R
+++ Bosley_Cybriwsky_Geier_Mekavibul_Project.R
@@ -35,16 +35,16 @@ hist(properties_2m$taxvaluedollarcnt,breaks=50,main="Distribution of house tax v
 hist(properties$taxvaluedollarcnt,breaks=1000,main="Distribution of all house tax valuations",xlab="Property Valuation (US dollars)")
 
 qqnorm(properties_2m$taxvaluedollarcnt,main="Normal Q-Q Plot of House Tax Valuations")
-abline(mean(properties_2m$taxvaluedollarcnt,na.rm=TRUE),sd(properties_700k$taxvaluedollarcnt,na.rm=TRUE),col='red')
+abline(mean(properties_2m$taxvaluedollarcnt,na.rm=TRUE),sd(properties_2m$taxvaluedollarcnt,na.rm=TRUE),col='red')
 
 # Testing whether or not the bedroom count of the house is correlated with the tax valuation of the house 
-t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==1],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==2])
-t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==1],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==3])
-t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==2],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==3])
+t.test(properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==1],properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==2])
+t.test(properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==1],properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==3])
+t.test(properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==2],properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==3])
 # p-value < 2.2e-16
 # Highly significant, p value < 0.05. The mean house valuation is different for 2 and 3 bedroom houses
 
-t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==6],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==8])
+t.test(properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==6],properties_2m$taxvaluedollarcnt[properties_2m$bedroomcnt==8])
 # p-value = 0.1498
 # Not significat, p value > 0.05. The difference in mean house valuation for houses of 6 and 8 bedrooms is not statistically significant.
 # This is probably a result of the number of houses having 6 and 8 bedrooms are very small.
