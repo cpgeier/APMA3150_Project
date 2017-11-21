fdbc3504405cacb98e61d221cb14cfd363216c0b
cpgeier <cpg3rb@virginia.edu>
2017-11-21 00:05:52 -0500

    cleaned file

--- APMAProject_C.Geier.R
+++ APMAProject_C.Geier.R
@@ -1,35 +1,42 @@
 properties <- read.csv("properties_2017.csv")
-properties[which(properties[,'numberofstories']==41),]
-properties2 <- properties[!is.na(properties$numberofstories),]
-properties2$taxamount
-properties3 <- properties2[!is.na(properties2$taxamount),]
-sort(properties3$taxamount)
-qqplot(properties3$numberofstories,properties3$taxamount)
-table(properties3$numberofstories)
-#      1      2      3      4      5      6      7      8     14     17     20     41 
-# 426471 243035  15708     68     18      4      2      1      1      1      1      1 
-summary(properties3[which(properties3[,'numberofstories']==1),]$taxamount)
-#Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
-#16.5    2238.6    3594.0    4510.9    5354.5 1840935.1 
-summary(properties3[which(properties3[,'numberofstories']==2),]$taxamount)
-#Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
-#17.7   3108.3   4750.7   5771.5   7017.4 686876.6 
-summary(properties3[which(properties3[,'numberofstories']==3),]$taxamount)
-#Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
-#79.4   2905.6   4731.2   6828.8   7381.1 845632.0 
-summary(properties3[which(properties3[,'numberofstories']==4),]$taxamount)
-#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#1763    7583   26685   82546   87919  533286 
-p4<-properties3[which(properties3[,'numberofstories']==4),]
-plot(p4$taxamount,p4$taxvaluedollarcnt)
-qqnorm(p4$taxvaluedollarcnt)
-qqplot(qexp(ppoints(length(p4$taxvaluedollarcnt))), p4$taxvaluedollarcnt)
-mean(properties3$taxvaluedollarcnt,na.rm=TRUE)
-#[1] 435499
-sd(properties3$taxvaluedollarcnt,na.rm=TRUE)
-#[1] 728364.6
-test_sub<-properties3[properties3$taxvaluedollarcnt<700000,]
-qqnorm(test_sub$taxvaluedollarcnt)
-qqline(test_sub$taxvaluedollarcnt)
-qqnorm(properties3$taxvaluedollarcnt[properties3$taxvaluedollarcnt<1000000])
-qqline(properties3$taxvaluedollarcnt[properties3$taxvaluedollarcnt<1000000])
+
+# Dimensions of dataset
+dim(properties)
+# [1] 2985217      58
+
+# Remove na entries
+properties <- properties[!is.na(properties$numberofstories),]
+properties <- properties[!is.na(properties$taxvaluedollarcnt),]
+
+# Dimensions of dataset
+dim(properties)
+#[1] 685640     58
+
+# Only include subset with similar properties
+# Some entries were not for single house and therefore were much larger
+# 700000 was chose as it was the point where the taxvaluedollarcnt value vs index
+# stopped being mostly linear. A different value could have been chosen,
+# but since this subset includes a great majority of the originial data 
+# excluding outliers, the subset is representative of the larger dataset.
+
+properties_700k <- properties[properties$taxvaluedollarcnt<500000,]
+
+hist(properties_700k$basementsqft)
+hist(properties_700k$bathroomcnt)
+hist(properties_700k$bedroomcnt)
+table(properties_700k$numberofstories)
+hist(properties_700k$taxvaluedollarcnt)
+
+qqnorm(properties_700k$taxvaluedollarcnt,main="Normal Q-Q Plot of House Tax Valuations")
+qqline(properties_700k$taxvaluedollarcnt,col='red')
+
+# Testing whether or not the bedroom count of the house is correlated with the tax valuation of the house 
+
+t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==2],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==3])
+# p-value < 2.2e-16
+# Highly significant, p value < 0.05. The mean house valuation is different for 2 and 3 bedroom houses
+
+t.test(properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==6],properties_700k$taxvaluedollarcnt[properties_700k$bedroomcnt==8])
+# p-value = 0.1498
+# Not significat, p value > 0.05. The difference in mean house valuation for houses of 6 and 8 bedrooms is not statistically significant.
+# This is probably a result of the number of houses having 6 and 8 bedrooms are very small.
